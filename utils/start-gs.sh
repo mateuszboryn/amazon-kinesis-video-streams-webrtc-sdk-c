c790b71c18b6c3310e6284dd3af9eed52b6f390a
Mateusz <mateusz.boryn@gmail.com>
2020-06-07 14:44:55 +0200

    start scripts

---
+++ utils/start-gs.sh
@@ -0,0 +1,9 @@
+export AWS_DEFAULT_REGION=eu-west-1
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+
+export AWS_KVS_VERBOSE=1
+export GST_DEBUG=3
+
+./kvsWebrtcClientMasterGstSample test audio-video
+# ./kvsWebrtcClientMasterGstSample test video-only
---
+++ utils/start.sh
@@ -0,0 +1,5 @@
+export AWS_DEFAULT_REGION=eu-west-1
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+
+./kvsWebrtcClientMaster test
